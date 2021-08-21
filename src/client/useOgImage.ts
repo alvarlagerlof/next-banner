42fe9e962ddccfc8e4f55deab9b4035baef510c2
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-08-22 01:13:55 +0200

    fix(useogimage hook): better undefined safety

--- src/client/useOgImage.ts
+++ src/client/useOgImage.ts
@@ -33,8 +33,8 @@ function getBase64(data: JsonMap) {
 export default function useOgImage(params: Options | undefined): Tag {
   const { asPath } = useRouter();
 
-  const layout = params?.layout ?? "default";
-  const data = params?.data ?? {};
+  const layout = params ? params.layout ?? "default" : "default";
+  const data = params ? params.data ?? {} : {};
 
   return {
     property: "og:image",
