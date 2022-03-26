50df427e2558c0edc76c4e5e9dd275ea7c26d5a8
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-03-26 13:39:44 +0100

    Rename folder

--- core/src/client/index.ts
+++ core/src/client/index.ts
@@ -2,7 +2,7 @@ import Provider from "./Provider";
 import Template from "./Template";
 import ScreenshotCanvas from "./ScreenshotCanvas";
 
-import useBannerData from "./useBannerData";
+import { useBannerData } from "./useBannerData";
 import setBannerData from "./setBannerData";
 
 import withBannerOptions from "./withBannerOptions";
--- core/src/client/useBannerData.ts
+++ core/src/client/useBannerData.ts
@@ -1,18 +1,7 @@
 import { useEffect, useState } from "react";
 import { Data } from "../types";
 
-// export default function getBannerData(): Data {
-//   if (
-//     typeof window !== "undefined" &&
-//     typeof window.NextBanner !== "undefined"
-//   ) {
-//     return window.NextBanner.data as Data;
-//   }
-
-//   return {} as Data;
-// }
-
-export default function useBannerData(): Data {
+export function useBannerData(): Data {
   const [data, setData] = useState<Data>({});
 
   useEffect(() => {
