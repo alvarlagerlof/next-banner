c7dad45b6f138e037c792d8ec1117a4baf49d1a1
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-04-01 11:43:42 +0200

    Rename example

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-+import Head from "next/head";
+import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
