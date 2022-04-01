7c9e84408bef635897932622e2b93f795e11f7bb
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-04-01 11:18:20 +0200

    Remove import

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
-import { setBannerData } from "next-banner";
-import Head from "next/head";
++import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
