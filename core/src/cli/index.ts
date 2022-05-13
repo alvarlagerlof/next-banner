cdff357fb2fe595d851f4397d4067fb8d9ca4ccf
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-05-13 19:59:43 +0200

    chore: upgrade dependecies

--- core/src/cli/index.ts
+++ core/src/cli/index.ts
@@ -33,53 +33,49 @@ export type LogsWithRoute = Array<{ route: string; message: string }>;
       }
     );
   })
-    .then(
-      async ({
-        result: {
-          results: [browser, server, routes],
-        },
-      }) => {
-        await task("Screenshot pages", async ({ task, setStatus, setOutput }) => {
-          let counter = 0;
-          const logs: LogsWithRoute = [];
+    .then(async ({ result: [browser, server, routes] }) => {
+      await task("Screenshot pages", async ({ task, setStatus, setOutput }) => {
+        let counter = 0;
+        const logs: LogsWithRoute = [];
 
-          async function doWork(iterator) {
-            for (const [, route] of iterator) {
-              const nestedTask = await task(`Route: ${route}`, async () => {
-                const extractData = new ExtractData(await browser.newPage());
-                await extractData.loadUrl(`http://localhost:${server.port}${route}`);
-                const { layout, meta, custom } = await extractData.extract();
-                await extractData.close();
+        async function doWork(iterator) {
+          for (const [, route] of iterator) {
+            const nestedTask = await task(`Route: ${route}`, async () => {
+              const extractData = new ExtractData(await browser.result.newPage());
+              await extractData.loadUrl(`http://localhost:${server.result.port}${route}`);
+              const { layout, meta, custom } = await extractData.extract();
+              await extractData.close();
 
-                const captureScreenshot = new CaptureScreenshot(await browser.newPage());
-                await captureScreenshot.insertData({ layout, meta, custom });
-                await captureScreenshot.loadUrl(`http://localhost:${server.port}/${layoutDir}/${layout}`);
-                await captureScreenshot.capture(route);
-                await captureScreenshot.close();
+              const captureScreenshot = new CaptureScreenshot(await browser.result.newPage());
+              await captureScreenshot.insertData({ layout, meta, custom });
+              await captureScreenshot.loadUrl(
+                `http://localhost:${server.result.port}/${layoutDir}/${layout}`
+              );
+              await captureScreenshot.capture(route);
+              await captureScreenshot.close();
 
-                // Add route to logs array
-                const combined = [...extractData.logs, ...captureScreenshot.logs];
-                const withRoute: LogsWithRoute = combined.map((log) => ({ route, message: log }));
-                logs.push(...withRoute);
-                setOutput(renderLogs(logs));
-              });
-              setStatus(`${++counter}/${routes.length}`);
-              nestedTask.clear();
-            }
+              // Add route to logs array
+              const combined = [...extractData.logs, ...captureScreenshot.logs];
+              const withRoute: LogsWithRoute = combined.map((log) => ({ route, message: log }));
+              logs.push(...withRoute);
+              setOutput(renderLogs(logs));
+            });
+            setStatus(`${++counter}/${routes.result.length}`);
+            nestedTask.clear();
           }
+        }
 
-          // Starts n workers sharing the same iterator
-          const iterator = routes.entries();
-          const workers = new Array(concurrency).fill(iterator).map(doWork);
-          await Promise.allSettled(workers);
+        // Starts n workers sharing the same iterator
+        const iterator = routes.result.entries();
+        const workers = new Array(concurrency).fill(iterator).map(doWork);
+        await Promise.allSettled(workers);
 
-          // Shut down processes
-        }).finally(async () => {
-          await browser.close();
-          server.serverProcess.kill("SIGINT");
-        });
-      }
-    )
+        // Shut down processes
+      }).finally(async () => {
+        await browser.result.close();
+        server.result.serverProcess.kill("SIGINT");
+      });
+    })
     .finally(async () => {
       process.exit();
     });
